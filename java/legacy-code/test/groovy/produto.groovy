aa0cdd1fca3a9656ed946b75f836ab098d4c6b94
Mauricio Borges Silva <mauricioborges@gmail.com>
2015-09-22 18:34:47 -0300

    added support to mock with Mockito (and related test)

--- java/legacy-code/test/groovy/produto.groovy
+++ java/legacy-code/test/groovy/produto.groovy
@@ -1,8 +1,19 @@
-
+import static org.mockito.Mockito.*
+import static org.mockito.Matchers.*
 import tdc2015.legacy.produto.ProdutoController
+import tdc2015.legacy.produto.ProdutoDAO
+
+import static org.mockito.Mockito.*
 
 class ProdutoControllerGroovyTesting extends GroovyTestCase {
-    void "test if it's possible to create a ProdutoController instance"(){
-        assert new ProdutoController() != null
+    void "test if it's possible to create a ProdutoController instance"() {
+        assert new ProdutoController(null) != null
+    }
+
+    void "test if it's possible to create a ProdutoController instance with a mocked DAO"() {
+        def mockedDAO = mock(ProdutoDAO.class)
+        new ProdutoController(mockedDAO).listar()
+        verify(mockedDAO, times(1)).list()
+
     }
 }
--- java/legacy-code/test/java/ProdutoControllerJavaTesting.java
+++ java/legacy-code/test/java/ProdutoControllerJavaTesting.java
@@ -1,13 +1,27 @@
 import junit.framework.TestCase;
-import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.Matchers.*;
 import org.junit.Test;
 import tdc2015.legacy.produto.ProdutoController;
+import tdc2015.legacy.produto.ProdutoDAO;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class ProdutoControllerJavaTesting extends TestCase {
 
     @Test
     public void testIfYouCanCreateAProdutoControllerInstance() {
-        assertThat(new ProdutoController(), is(not(nullValue())));
+        assertThat(new ProdutoController(null), is(not(nullValue())));
+    }
+
+    @Test
+    public void testIfYouCanCreateAProdxutoControllerInstanceWithAMockedDAO() {
+        ProdutoDAO mockedDAO = mock(ProdutoDAO.class);
+        new ProdutoController(mockedDAO).listar();
+        verify(mockedDAO, times(1)).list();
+
     }
 }
