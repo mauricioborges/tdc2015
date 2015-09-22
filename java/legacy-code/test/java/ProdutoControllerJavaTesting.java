9c9fc4cc6bb0af38610f90becc936931d8ff7edd
Mauricio Borges Silva <mauricioborges@gmail.com>
2015-09-22 16:08:06 -0300

    added hamcrest support for java

--- java/legacy-code/test/java/ProdutoControllerJavaTesting.java
+++ java/legacy-code/test/java/ProdutoControllerJavaTesting.java
@@ -1,4 +1,6 @@
 import junit.framework.TestCase;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
 import org.junit.Test;
 import tdc2015.legacy.produto.ProdutoController;
 
@@ -9,6 +11,6 @@ public class ProdutoControllerJavaTesting extends TestCase {
 
     @Test
     public void testIfYouCanCreateAProdutoControllerInstance() {
-        assertNotNull(new ProdutoController());
+        assertThat(new ProdutoController(), is(not(nullValue())));
     }
 }
